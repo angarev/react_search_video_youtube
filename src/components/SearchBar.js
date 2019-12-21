7bc437d34594a2e7a8a12ca268164973ccb2c0d1
Panayot Angarev <angarev@abv.bg>
2019-12-21 09:56:26 +0200

    Basic search interface to find youtube videos

---
+++ src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+	state = { term: "" };
+
+	onInputChange = event => {
+		this.setState({ term: event.target.value });
+	};
+
+	onFormSubmit = event => {
+		event.preventDefault();
+
+		//TODO Make sure we call
+		//callback from parent component
+		this.props.onFormSubmit(this.state.term);
+	};
+
+	render() {
+		return (
+			<div className="search-bar ui segment">
+				<form onSubmit={this.onFormSubmit} className="ui form">
+					<label htmlFor="videoSearch">Search video</label>
+					<input
+						type="text"
+						name="videoSearch"
+						value={this.state.term}
+						onChange={this.onInputChange}
+					/>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default SearchBar;
